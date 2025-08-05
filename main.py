9313865b43f1713ded0bc98ca357ac0b30ab42ef
Matchyman <jmatcham31@googlemail.com>
2025-08-05 13:38:19 +0100

    Added --verbose to CLI to display less information if false

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from google.genai import types
 
 load_dotenv()
 
-def main(arg):
+def main(arg, verbose):
     print("Hello from aiagentproject!")
     messages = [
     types.Content(role="user", parts=[types.Part(text=arg)]),
@@ -17,17 +17,21 @@ def main(arg):
     model='gemini-2.0-flash-001', 
     contents=messages
     )
-    
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {messages}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
     args = sys.argv
+    verbose = False
     if len(args) > 1:
         arg = args[1]
+        if len(args) > 2:
+            verbose = "--verbose" in args[2]
     else:
         print("No prompt entered, please enter prompt into CLI when running programme")
         sys.exit(1)
-    main(arg)
+    main(arg, verbose)
