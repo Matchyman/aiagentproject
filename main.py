043eca9675cd6246adfb7703757b4ff9095efc17
Matchyman <jmatcham31@googlemail.com>
2025-08-05 11:46:16 +0100

    Store CLI args in list

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 
-
 def main(arg):
     print("Hello from aiagentproject!")
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=arg)]),
+]
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)  
     response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+    model='gemini-2.0-flash-001', 
+    contents=messages
+    )
     
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
