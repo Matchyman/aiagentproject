aedca58eddafaadfe9c1d575aae802df644eb5af
Matchyman <jmatcham31@googlemail.com>
2025-08-06 14:54:04 +0100

    Create functions folder and add functions for reading, writing and checking file content

---
+++ functions/get_file_content.py
@@ -0,0 +1,20 @@
+import os
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    working_direct_abs_path = os.path.abspath(working_directory)
+    filepath = os.path.join(working_direct_abs_path, file_path)
+    file_path_abs = os.path.abspath(filepath)
+    if not file_path_abs.startswith(working_direct_abs_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(file_path_abs):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(file_path_abs, "r") as file:
+            content_string = file.read(MAX_CHARS)
+    except Exception as e:
+        return(f"Error: {e}")
+    if len(content_string) == MAX_CHARS:
+        content_string += f"[...File '{file_path}' truncated at '{MAX_CHARS}' characters]"
+    return content_string
---
+++ functions/write_file.py
@@ -0,0 +1,14 @@
+import os
+def write_file(working_directory, file_path, content):
+    working_direct_abs_path = os.path.abspath(working_directory)
+    filepath = os.path.join(working_direct_abs_path, file_path)
+    file_path_abs = os.path.abspath(filepath)
+    if not file_path_abs.startswith(working_direct_abs_path):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    try:
+        with open(file_path_abs, "w") as f:
+            f.write(content)
+    except Exception as e:
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        
