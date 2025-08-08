b50685c35d0c1bd13e2540218ec5fccf932e599e
Matchyman <jmatcham31@googlemail.com>
2025-08-08 10:33:05 +0100

    added tools to agent calls, specifiying which are avalible, ouputing any function calls made. Updated gitignore

--- main.py
+++ main.py
@@ -3,20 +3,42 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
 
 def main(arg, verbose):
     print("Hello from aiagentproject!")
+
     messages = [
     types.Content(role="user", parts=[types.Part(text=arg)]),
-]
+    ]
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)  
     response = client.models.generate_content(
     model='gemini-2.0-flash-001', 
-    contents=messages
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=system_prompt),
+    tools = [available_functions]
     )
+    if response.function_calls:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
     print(response.text)
     if verbose:
         print(f"User prompt: {messages}")
