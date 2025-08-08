a420b184674aa6059303a485b138b0ca251ddfeb
Matchyman <jmatcham31@googlemail.com>
2025-08-08 11:42:42 +0100

    Added schema to other functions and updated main.py to reflect

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,5 +1,6 @@
 import os
 from config import MAX_CHARS
+from google.genai import types
 
 
 def get_file_content(working_directory, file_path):
@@ -18,3 +19,17 @@ def get_file_content(working_directory, file_path):
     if len(content_string) == MAX_CHARS:
         content_string += f"[...File '{file_path}' truncated at '{MAX_CHARS}' characters]"
     return content_string
+
+schema_get_file_content = types.FunctionDeclaration(
+    name="get_file_content",
+    description="Reads files in the specified file constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The file that is to be read.",
+            ),
+        },
+    ),
+)
--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 def write_file(working_directory, file_path, content):
     working_direct_abs_path = os.path.abspath(working_directory)
     filepath = os.path.join(working_direct_abs_path, file_path)
@@ -11,3 +12,23 @@ def write_file(working_directory, file_path, content):
     except Exception as e:
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Writes file in the specified directory, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to be written to",
+            ),
+            "content": types.Schema(
+                type=types.Type.STRING,
+                description="The content to be written to the file"
+            )
+        },
+        required=["file_path", "content"]
+    ),
+)
